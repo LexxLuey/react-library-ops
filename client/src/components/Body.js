38ba5321469daeeb32ec955e72628f7a302ac627
LexxLuey <biggestluey@gmail.com>
2021-05-12 20:09:45 +0100

    finished
    completion

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Body.css';
 import Header from './Header';
 import { useDataLayerValue } from "../DataLayer";
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@material-ui/icons';
+import SongRow from "./SongRow";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({spotify}) {
     const [{discover_weekly}, dispatch] = useDataLayerValue()
@@ -21,6 +26,19 @@ function Body({spotify}) {
                     <h2>Discover Weekly</h2>
                     <p>{discover_weekly?.description}</p>
                 </div>
+                <div className="body_songs">
+                    <div className="body_icons">
+                        <PlayCircleFilledIcon className="body_shuffle" />
+                        <FavoriteIcon fontSize="large" />
+                        <MoreHorizIcon />
+                    </div>
+
+                    {discover_weekly?.tracks.items.map((item) =>(
+                        <SongRow track={item.track} />
+                    )) }
+                </div>
+                
+                
             </div>
         </div>
     )
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -3,7 +3,9 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    token: "BQDm96z3Lv2tltf4Wee3HzsKgMffAI-XmvRCqVytdY3Jv2P360sG376wYxVxGNbbpKYrXOYg73DsRtoG-zDnB5qPVb3Nb0JR1QqrUQFeEP0PN--qPP-rYZATWr91jgCLYgFmMAanlYuIetuylyTxP1vRl2EPjCcoV1vYwdAqJ3OUOxkAtfTu",
+    token: null,
+
+    token_dev: "BQDm96z3Lv2tltf4Wee3HzsKgMffAI-XmvRCqVytdY3Jv2P360sG376wYxVxGNbbpKYrXOYg73DsRtoG-zDnB5qPVb3Nb0JR1QqrUQFeEP0PN--qPP-rYZATWr91jgCLYgFmMAanlYuIetuylyTxP1vRl2EPjCcoV1vYwdAqJ3OUOxkAtfTu",
 };
 
 const reducer = (state, action) => {
